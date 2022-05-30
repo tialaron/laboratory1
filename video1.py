b26a976754831691a485a292a84ead39de958607
tialaron <30555530+tialaron@users.noreply.github.com>
2022-05-30 14:41:13 +0300

    Update video1.py

--- video1.py
+++ video1.py
@@ -1,6 +1,9 @@
-import pandas as pd
-import streamlit as st
+import matplotlib.pyplot as plt #Отрисовка графиков
+import seaborn as sns
+import numpy as np #Numpy
 from PIL import Image
+import time
+from datetime import datetime
 
 st.set_page_config(layout="wide")
 st.title('Лабораторная работа "Распознавание рукописных цифр". ')
