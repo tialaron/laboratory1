dc2ace7921c7c1979292702d1a5299ef5678963f
tialaron <30555530+tialaron@users.noreply.github.com>
2022-06-01 17:05:36 +0300

    Update laboratorymnist1.py

--- laboratorymnist1.py
+++ laboratorymnist1.py
@@ -110,7 +110,7 @@ with st.expander('Пункт 7.'):
               tgt1 = np.squeeze(imgData1)
               st.write(tgt1.shape)
               im111 = Image.fromarray(tgt1)
-              im111.save(file_path)
+              #im111.save(file_path)
               st.image(file_path)
               y_predict1 = model_2d.predict(imgData1) 
               y_maxarg = np.argmax(y_predict1, axis=1)
