9e12203af4a770a8439122008823224adcd5f298
tialaron <30555530+tialaron@users.noreply.github.com>
2022-06-02 10:15:33 +0300

    Update laboratorymnist1.py

--- laboratorymnist1.py
+++ laboratorymnist1.py
@@ -160,7 +160,7 @@ with st.expander('Пункт 9.'):
     with col10:
             st.write('Фильтр Гаусса',value_gaus)
             image222 = Image.open(file_path)
-            im2 = image222.filter(ImageFilter.GaussianBlur(radius = value_gauss))
+            im2 = image222.filter(ImageFilter.GaussianBlur(radius = value_gaus))
             im2.save(file_path)
             st.image(file_path)
 with st.expander('Пункт 10'):
