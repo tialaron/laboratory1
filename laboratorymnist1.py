939318ccca490a2538b1b7d6cc29aff9644beeca
tialaron <30555530+tialaron@users.noreply.github.com>
2022-06-14 16:14:23 +0300

    Update laboratorymnist1.py

--- laboratorymnist1.py
+++ laboratorymnist1.py
@@ -204,10 +204,8 @@ with st.expander('Пункт 12.'):
             min_img_color = i3.min()
             THRESHOLD_VALUE = (mid_img_color - (mid_img_color - min_img_color) * step_lobe)
             thresholdedData = (i3 < THRESHOLD_VALUE) * 1.0
-            st.write(thresholdedData)
             imm1 = Image.fromarray(thresholdedData)
-            imm1.save(file_path)
-            st.image(file_path)
+            st.write(imm1) 
             #i4 = np.expand_dims(thresholdedData, axis=0)
             #imm1 = Image.fromarray(i4) 
             #imm1.save(file_path)
